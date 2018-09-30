13beda25ee2e8b5c2c3c4855e36e6c7c6d106a69
Stepan Rakhimov <stepan.rakhimov@4cinsights.com>
2018-09-30 21:44:36 +0200

    Initial Commit

---
+++ betserver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  betserver
+//
+//  Created by Stepan Rakhimov on 30/09/2018.
+//  Copyright © 2018 Stepan Rakhimov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
