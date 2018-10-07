ddf8979981668e39528a4fa01fa11c28914370b1
Stepan Rakhimov <stepan.rakhimov@4cinsights.com>
2018-10-07 17:08:56 +0200

    refactoring main func

--- betserver/main.c
+++ betserver/main.c
@@ -46,13 +46,13 @@ int generate_unique_id()
   return rand() % BETSERVER_CLIENT_ID_MAX;
 }
 
-/** zero clients array */
+/* zero clients array */
 void init_clients()
 {
   bzero(clients, sizeof(clients));
 }
 
-/** returns first unused index or -1 if not found */
+/* returns first unused index or -1 if not found */
 int get_free_client_index()
 {
   for(int i=0; i<BETSERVER_NUM_CLIENTS; ++i)
@@ -63,7 +63,7 @@ int get_free_client_index()
   return -1;
 }
 
-/** returns client index for unique id, -1 if not found */
+/* returns client index for unique id, -1 if not found */
 int get_client_index_by_id(int unique_id)
 {
   for (int i=0; i<BETSERVER_NUM_CLIENTS; ++i)
@@ -73,7 +73,7 @@ int get_client_index_by_id(int unique_id)
   return -1;
 }
 
-/** returns client index for unique id, -1 if not found */
+/* returns client index for unique id, -1 if not found */
 int get_client_index_by_socketfd(int socket)
 {
   for (int i=0; i<BETSERVER_NUM_CLIENTS; ++i)
@@ -83,7 +83,7 @@ int get_client_index_by_socketfd(int socket)
   return -1;
 }
 
-/** returns client unique id for socket fd, -1 if not found */
+/* returns client unique id for socket fd, -1 if not found */
 int get_client_uniqueid_by_socketfd(int socket)
 {
   for (int i=0; i<BETSERVER_NUM_CLIENTS; ++i)
@@ -113,7 +113,7 @@ int count_betting_clients()
   return res;
 }
 
-/** returns client's unique id or -1 if failed */
+/* returns client's unique id or -1 if failed */
 int create_client(int socket_fd)
 {
   int index = get_free_client_index();
@@ -130,7 +130,7 @@ int create_client(int socket_fd)
   return new_id;
 }
 
-/** returns client's index or -1 if not found */
+/* returns client's index or -1 if not found */
 int set_client_bet(int unique_id, unsigned int bet)
 {
   int index = get_client_index_by_id(unique_id);
@@ -194,7 +194,16 @@ void term(int signum)
   done = 1;
 }
 
-//void set_nonblock(int socket) {
+//void set_socket_reuse(int socket)
+//{
+//  int yes = 1;
+//  if(setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+//  {
+//    error("setsockopt failed");
+//  }
+//}
+//
+//void set_socket_nonblock(int socket) {
 //  int flags;
 //  flags = fcntl(socket, F_GETFL, 0);
 //  fcntl(socket, F_SETFL, flags | O_NONBLOCK);
@@ -205,221 +214,247 @@ unsigned int roll_the_dice()
   return BETSERVER_NUM_MIN + rand() / (RAND_MAX / (BETSERVER_NUM_MAX - BETSERVER_NUM_MIN + 1) + 1);
 }
 
-int main(int argc, const char * argv[]) {
-  puts("Betserver started.");
-  fflush(stdout);
+void setup_sigterm_handling()
+{
   struct sigaction action;
   bzero(&action, sizeof(struct sigaction));
   action.sa_handler = term;
   sigaction(SIGTERM, &action, NULL);
-  
-  
-  int listen_socket;
-  struct sockaddr_in server;
-  char buffer[BETSERVER_MESSAGE_SIZE_MAX];
-  init_clients();
-  
-  /* master file descriptor list */
-  fd_set master;
-  /* temp file descriptor list for select() */
-  fd_set read_fds;
-  /* maximum file descriptor number */
-  int fdmax;
-  /* clear the master and temp sets */
-  FD_ZERO(&master);
-  FD_ZERO(&read_fds);
-  
-  unsigned int winner = 0;
-  //  struct client* clients[BETSERVER_NUM_CLIENTS];  // TODO: SR
-  
-  /** Create streaming socket */
-  if ((listen_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+}
+
+int init_server_socket(fd_set* sockets, int* sockets_size)
+{
+  int server_socket;
+
+  /* Create streaming socket */
+  if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     error("socket create failed");
   }
-  
-  /** Initialize address/port structure */
+
+  /* Initialize address/port structure */
+  struct sockaddr_in server;
   bzero(&server, sizeof(server));
   server.sin_family = AF_INET;
   server.sin_port = htons(BETSERVER_PORT);
   server.sin_addr.s_addr = INADDR_ANY;
-  
-  /** Assign a port number to the socket */
-  if (bind(listen_socket, (struct sockaddr*)&server, sizeof(server)) != 0)
+
+  /* Assign a port number to the socket */
+  if (bind(server_socket, (struct sockaddr*)&server, sizeof(server)) != 0)
   {
     error("socket:bind failed");
   }
-  
-  /** Make it a "listening socket". Limit to BETSERVER_NUM_CLIENTS connections */
-  if (listen(listen_socket, BETSERVER_NUM_CLIENTS) != 0)
+
+  /* Make it a "listening socket". Limit to BETSERVER_NUM_CLIENTS connections */
+  if (listen(server_socket, BETSERVER_NUM_CLIENTS) != 0)
   {
     error("socket:listen failed");
   }
-//  int yes = 1;
-//  if(setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-//  {
-//    error("setsockopt failed");
-//  }
 
-//  set_nonblock(listen_socket);
-  
   /* add the listener to the master set */
-  FD_SET(listen_socket, &master);
+  FD_SET(server_socket, sockets);
   /* keep track of the biggest file descriptor */
-  fdmax = listen_socket; /* so far, it's this one*/
-  
-  /** save current time for later use */
+  *sockets_size = server_socket; /* so far, it's this one*/
+
+  return server_socket;
+}
+
+void handle_winner(const unsigned int winner, const int listen_socket, fd_set* sockets, const int sockets_size) {
+  for(int j = 0; j <= sockets_size; j++)
+  {
+    /* send to everyone! */
+    if(FD_ISSET(j, sockets) && (j != listen_socket))
+    {
+      char win[50];
+      sprintf(win, "winner is %u\n", winner);
+      if(send(j, win, strlen(win), 0) == -1)
+//      char buffer[sizeof(winner)];
+//      *buffer = winner;
+//      if(send(j, (const char*) &winner, sizeof(winner), 0) == -1)
+      {
+        perror("send failed");
+      }
+      /* forget client */
+      int index = get_client_index_by_socketfd(j);
+      if (-1 == index)
+      {
+        close(j);
+        FD_CLR(j, sockets);
+      }
+      else
+      {
+        if(clients[index].bet_done)
+        {
+          if(clients[index].bet == winner)
+          {
+            printf("client %d has won\n", clients[index].unique_id);
+            char* msg = "You have won!\n";
+            if(send(j, msg, strlen(msg), 0) == -1)
+            {
+              perror("send failed");
+            }
+          }
+          remove_client_by_index(index);
+          close(j);
+          FD_CLR(j, sockets);
+        }
+      }
+    }
+  }
+}
+
+void handle_updated_sockets(fd_set* sockets, const fd_set* sockets_updated, int* sockets_size, const int listen_socket)
+{
+  /* message buffer */
+  char buffer[BETSERVER_MESSAGE_SIZE_MAX];
+  printf("sockets size: %d\n", *sockets_size);
+
+  for(int i = 0; i <= *sockets_size; i++)
+  {
+    if(FD_ISSET(i, sockets_updated))
+    { /* we got one... */
+      if(i == listen_socket)
+      {
+        int client_socket;
+        struct sockaddr_in client_addr;
+        bzero(&client_addr, sizeof(client_addr));
+        socklen_t addrlen=sizeof(client_addr);
+
+        /* accept an incomming connection  */
+        client_socket = accept(listen_socket, (struct sockaddr*)&client_addr, &addrlen);
+        if (client_socket == -1)
+        {
+          perror("client connect failed");
+        }
+        else
+        {
+          FD_SET(client_socket, sockets); /* add to master set */
+          if(client_socket > *sockets_size)
+          { /* keep track of the maximum */
+            *sockets_size = client_socket;
+          }
+          printf("%s:%d connected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+          if (-1 == create_client(client_socket))
+          {
+            close(client_socket);
+            FD_CLR(client_socket, sockets);
+            perror("client create failed, disconnect");
+          }
+        }
+      }
+      else
+      {
+        /* handle data from a client */
+        size_t nbytes;
+        if((nbytes = recv(i, buffer, sizeof(buffer), 0)) <= 0)
+        {
+          /* got error or connection closed by client */
+          if(nbytes == 0)
+          /* connection closed */
+            printf("socket %d hung up\n", i);
+          else
+            perror("recv failed");
+
+          /* close it... */
+          close(i);
+          /* remove from master set */
+          FD_CLR(i, sockets);
+          /* forget client */
+          remove_client_by_socketfd(i);
+        }
+        else
+        {
+          if (-1 == handle_socket_message(i, buffer, nbytes))
+          {
+            /* close it... */
+            close(i);
+            /* remove from master set */
+            FD_CLR(i, sockets);
+          }
+        }
+      }
+    }
+  }
+}
+
+static int check_updated_sockets(fd_set *sockets, fd_set *sockets_updated, int sockets_size) {
+  /* set timeout for select() */
+  struct timeval select_timeout;
+  bzero(&select_timeout, sizeof(select_timeout));
+  select_timeout.tv_usec = BETSERVER_TICK_MS * 1000;
+
+  /* copy open sockets */
+  FD_COPY(sockets, sockets_updated);
+  /* get updated sockets */
+  int selected = select(sockets_size+1, sockets_updated, NULL, NULL, &select_timeout);
+  if(selected == -1)
+  {
+    error("select failed");
+  }
+  return selected;
+}
+
+int main(int argc, const char * argv[]) {
+  puts("Betserver started.");
+  fflush(stdout);
+
+  /* handle SIGTERM */
+  setup_sigterm_handling();
+
+  /* initialize clients storage */
+  init_clients();
+
+  /* sockets descriptor set */
+  fd_set sockets;
+  /* helper sockets descriptor set */
+  fd_set sockets_updated;
+  /* current sockets set size */
+  int sockets_size;
+  /* clear the master and temp sets */
+  FD_ZERO(&sockets);
+  FD_ZERO(&sockets_updated);
+
+  /* server socket */
+  int listen_socket = init_server_socket(&sockets, &sockets_size);
+//  set_socket_reuse(listen_socket);
+//  set_socket_nonblock(listen_socket);
+
+  /* save current time for later use */
   time_t previous_time;
   time(&previous_time);
   
-  /** Server runs till SIGTERM */
+  /* Server runs till SIGTERM */
   while (!done)
   {
     fflush(stdout);
+
     time_t current_time;
-    time(&current_time);
-    double seconds = difftime(current_time, previous_time);
+    double seconds = difftime(time(&current_time), previous_time);
     if (seconds >= BETSERVER_BET_PERIOD)
     {
       printf("%.1f sec passed\n", seconds);
       previous_time = current_time;
       if (count_betting_clients() > 0)
       {
-        winner = roll_the_dice();
+        /* get winner */
+        unsigned int winner = roll_the_dice();
         printf("winner is %u\n", winner);
 
-        /* we got winner send it */
-        for(int j = 0; j <= fdmax; j++)
-        {
-          /* send to everyone! */
-          if(FD_ISSET(j, &master) && (j != listen_socket))
-          {
-            char win[50];
-            sprintf(win, "winner is %u\n", winner);
-            if(send(j, win, strlen(win), 0) == -1)
-//            char buffer[sizeof(winner)];
-//            *buffer = winner;
-//            if(send(j, (const char*) &winner, sizeof(winner), 0) == -1)
-            {
-              perror("send failed");
-            }
-            /* forget client */
-            int index = get_client_index_by_socketfd(j);
-            if (-1 == index)
-            {
-              close(j);
-              FD_CLR(j, &master);
-            }
-            else
-            {
-              if(clients[index].bet_done)
-              {
-                if(clients[index].bet == winner)
-                {
-                  printf("client %d has won\n", clients[index].unique_id);
-                  char* msg = "You have won!\n";
-                  if(send(j, msg, strlen(msg), 0) == -1)
-                  {
-                    perror("send failed");
-                  }
-                }
-                remove_client_by_index(index);
-                close(j);
-                FD_CLR(j, &master);
-              }
-            }
-          }
-        }
+        /* send winner around */
+        handle_winner(winner, listen_socket, &sockets, sockets_size);
       }
     }
     
-    /* copy it */
-    read_fds = master;
-
-    /** set timeout for select() */
-    struct timeval select_timeout;
-    bzero(&select_timeout, sizeof(select_timeout));
-    select_timeout.tv_usec = BETSERVER_TICK_MS * 1000;
-
-    int selected = 0;
-    selected = select(fdmax+1, &read_fds, NULL, NULL, &select_timeout);
-    if(selected == -1)
-    {
-      error("select failed");
-    }
-    if (selected == 0) {
-      continue;
-    }
+    int selected = check_updated_sockets(&sockets, &sockets_updated, sockets_size);
+    if (selected == 0) continue;
 
     /*run through the existing connections looking for data to be read*/
-    for(int i = 0; i <= fdmax; i++)
-    {
-      if(FD_ISSET(i, &read_fds))
-      { /* we got one... */
-        if(i == listen_socket)
-        {
-          int client_socket;
-          struct sockaddr_in client_addr;
-          bzero(&client_addr, sizeof(client_addr));
-          socklen_t addrlen=sizeof(client_addr);
-          
-          /** accept an incomming connection  */
-          client_socket = accept(listen_socket, (struct sockaddr*)&client_addr, &addrlen);
-          if (client_socket == -1)
-          {
-            perror("client connect failed");
-          }
-          else{
-            FD_SET(client_socket, &master); /* add to master set */
-            if(client_socket > fdmax)
-            { /* keep track of the maximum */
-              fdmax = client_socket;
-            }
-            printf("%s:%d connected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-            if (-1 == create_client(client_socket))
-            {
-              close(client_socket);
-              FD_CLR(client_socket, &master);
-              perror("client create failed, disconnect");
-            }
-          }
-        }
-        else{
-          /* handle data from a client */
-          size_t nbytes;
-          if((nbytes = recv(i, buffer, sizeof(buffer), 0)) <= 0)
-          {
-            /* got error or connection closed by client */
-            if(nbytes == 0)
-            /* connection closed */
-              printf("socket %d hung up\n", i);
-            else
-              perror("recv failed");
-            
-            /* close it... */
-            close(i);
-            /* remove from master set */
-            FD_CLR(i, &master);
-            /* forget client */
-            remove_client_by_socketfd(i);
-          }
-          else
-          {
-            if (-1 == handle_socket_message(i, buffer, nbytes))
-            {
-              /* close it... */
-              close(i);
-              /* remove from master set */
-              FD_CLR(i, &master);
-            }
-          }
-        }
-      }
-    }
+    handle_updated_sockets(&sockets, &sockets_updated, &sockets_size, listen_socket);
   }
   
-  /** Clean up */
+  /* Clean up */
   close(listen_socket);
+  puts("Betserver stopped.");
 
   return 0;
 }
